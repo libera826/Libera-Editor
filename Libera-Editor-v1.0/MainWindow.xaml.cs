2f45f4b2d25fc3c1885d28dd2fd1dc644b3b190e
Craig <libera826@gmail.com>
2017-08-12 03:14:53 +0900

    레포지토리 갈음. 파일 열기/저장 가능해 짐

---
+++ Libera-Editor-v1.0/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Libera_Editor_v1._0
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void fileOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|Libera Editor Setting files (*.lbs)|*.lbs|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                textArea.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void fileSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|Libera Editor Setting file (*.lbs)|*.lbs|All file (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, textArea.Text);
+
+        }
+    }
+}
